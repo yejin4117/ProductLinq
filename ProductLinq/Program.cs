c7df19e66186aff214d3954a4d895f9768421392
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-08 10:59:15 +0900

    Linq를 적용하여 데이터 조건에 따른 추출 및 정렬

--- ProductLinq/Program.cs
+++ ProductLinq/Program.cs
@@ -8,8 +8,27 @@ namespace ProductLinq
 {
     internal class Program
     {
+        static private List<Product> input = new List<Product>()
+        {
+            new Product(){ Name="고구마", Price=2000 },
+            new Product(){ Name="사과", Price=1500 },
+            new Product(){ Name="바나나", Price=3000 },
+            new Product(){ Name="배", Price=3500 },
+            new Product(){ Name="감자", Price=800 },
+            new Product(){ Name="토마토", Price=1000 },
+            new Product(){ Name="옥수수", Price=2000 },
+            new Product(){ Name="자두", Price=800 },
+        };
         static void Main(string[] args)
         {
+            var output = from item in input
+                         where item.Price > 1500
+                         orderby item.Name
+                         select item;
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
