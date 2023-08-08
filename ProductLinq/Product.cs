f4b938f53556aafc04b7ef722fc7af7012b0d040
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-08 10:46:48 +0900

    Product 클래스 추가

---
+++ ProductLinq/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLinq
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            //감자: 500원
+            return Name + ": " + Price + "원";
+        }
+    }
+}
